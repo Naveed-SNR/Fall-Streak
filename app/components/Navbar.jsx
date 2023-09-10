dc47c56f37ae94b3a2bbd90f96268ca0c26637c9
Naveed-SNR <sheikhnaveed3660@gmail.com>
2023-09-10 21:49:48 +0530

    Fixed misalignment of navbar links, icons and hamburger menu icon

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Logo from '../../public/images/logo/Logo.svg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faImage, faTh, faEnvelope, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faImage, faTh, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { Fade as Hamburger } from 'hamburger-react';
 
 export default function Navbar() {
@@ -13,9 +13,9 @@ export default function Navbar() {
   const toggleSidebar=()=> setSidebar(!sidebar)
   return (
     <main>
-      <nav className="w3-top navbar-expand-lg w3-top">
+      <nav className="w3-top w3-card">
                  
-        <div className="w3-bar w3-card">
+        <div className="w3-bar">
         <Link href="/#home" className="w3-bar-item w3-wide">
             <Image
               className="animate"
@@ -27,22 +27,22 @@ export default function Navbar() {
               left={0}
             />
           </Link>
-          <div className="w3-right mt-3" id="navbarSupportedContent">
-                <Link href="/#about" className="w3-bar-item w3-button me-2 d-none d-lg-flex">ABOUT</Link>
+          <div className="w3-right" id="navbarSupportedContent">
+                <Link href="/#about" className="w3-bar-item w3-button w3-round me-2 d-none d-lg-flex mt-3">ABOUT</Link>
 
-                <Link href="/gallery" className="w3-bar-item w3-button d-none d-lg-flex">
-                  <FontAwesomeIcon icon={faImage} style={{ fontSize: '1rem' }} className="me-2" /> GALLERY
+                <Link href="/gallery" className="w3-bar-item w3-button d-none d-lg-flex mt-3">
+                  <FontAwesomeIcon icon={faImage} style={{ fontSize: '1rem' }} className="me-2 mt-1" /> GALLERY
                 </Link>
 
-                <Link href="/#packages" className="w3-bar-item w3-button d-none d-lg-flex">
-                  <FontAwesomeIcon icon={faTh} style={{ fontSize: '1rem' }} className="me-2" /> PACKAGES
+                <Link href="/#packages" className="w3-bar-item w3-button w3-round d-none d-lg-flex mt-3">
+                  <FontAwesomeIcon icon={faTh} style={{ fontSize: '1rem' }} className="me-2 mt-1" /> PACKAGES
                 </Link>
 
-                <Link href="/#contact" className="w3-bar-item w3-button d-none d-lg-flex">
-                  <FontAwesomeIcon icon={faEnvelope} style={{ display: 'inline' }} className="me-2" /> CONTACT
+                <Link href="/#contact" className="w3-bar-item w3-button w3-round d-none d-lg-flex mt-3">
+                  <FontAwesomeIcon icon={faEnvelope} style={{ display: 'inline' }} className="me-2 mt-1" /> CONTACT
                 </Link>
-                <div className="d-lg-none d-flex" onClick={toggleSidebar}>
-                  <Hamburger direction="right" />
+                <div className="d-lg-none d-block mt-2" onClick={toggleSidebar}>
+                  <Hamburger rounded size={21} direction="right" label="Toggle Sidebar" />
                 </div>
           </div>
         </div>
@@ -50,7 +50,7 @@ export default function Navbar() {
         {/* Sidebar */}
         <div className={sidebar ? 'd-block' : 'd-none'}  >
 
-            <div className="w3-sidebar w3-bar-block w3-animate-right w3-round">
+            <div className="w3-sidebar w3-bar-block w3-animate-right w3-round w3-card">
             <Link href="/#about" className="w3-bar-item w3-button me-2">ABOUT</Link>
               
               <Link href="/gallery" className="w3-bar-item w3-button">
