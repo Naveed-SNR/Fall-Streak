06f2c1c4e01cfc1b45c91598f77963ded3e8c34d
Naveed-SNR <sheikhnaveed3660@gmail.com>
2023-09-10 16:15:12 +0530

    Added animated hamburger icon for toggling navbar on smaller screens

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,76 +1,63 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import Logo from '../../public/images/logo/Logo.svg'
+"use client"
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from '../../public/images/logo/Logo.svg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faImage, faTh, faEnvelope, faBars } from "@fortawesome/free-solid-svg-icons"; 
-
+import { faImage, faTh, faEnvelope, faBars } from "@fortawesome/free-solid-svg-icons";
+import { Fade as Hamburger } from 'hamburger-react';
 
 export default function Navbar() {
-    return (
-        <main>
-            {/* Navbar (sit on top) */}
-            <nav className="w3-top fw-medium">
-                <div className="w3-bar w3-card" id="myNavbar" >
-                    <Link href="/#home" className="w3-bar-item w3-wide">
-                        <Image className="animate"
-                            src={Logo}
-                            alt="Logo"
-                            width={111}
-                            position="fixed"
-                            top={0}
-                            left={0}
-                        />
-                    </Link>
-                    
-                    <div className="w3-right p-2 mt-1">
-
-                        <Link href="/#about" className="w3-bar-item w3-button me-2 d-none d-lg-block">ABOUT</Link>
-                        
-                        <Link href="/gallery" className="w3-bar-item w3-button d-none d-lg-block">
-                            <FontAwesomeIcon icon={faImage} style={{ fontSize: '1rem' }} className="me-2" /> GALLERY
-                        </Link> 
-                        
-                        <Link href="/#packages" className="w3-bar-item w3-button d-none d-lg-block">
-                            <FontAwesomeIcon icon={faTh} style={{ fontSize: '1rem' }} className="me-2" /> PACKAGES
-                        </Link>
-                        <Link href="/#contact" className="w3-bar-item w3-button d-none d-lg-block">
-                            <FontAwesomeIcon icon={faEnvelope} style={{ display: 'inline' }} className="me-2" /> CONTACT
-                        </Link>
-                        
-                        <button id="#navbarToggler" className="btn mt-1 d-block d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-labelledby="navbarToggler" aria-controls="offcanvasRight">
-                        <FontAwesomeIcon icon={faBars} size="xl" style={{ display: 'inline', right: '0px' }} className="me-2" /> 
-                    </button>
-                    </div>
-                </div>
-            </nav>
-            
-            <div className="d-lg-none">
-                
-                <div className="w3-round-medium offcanvas offcanvas-end bg-transparent" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-  
-                  <div className="offcanvas-body fw-regular">
-                  <div className="d-flex justify-content-between">
-                    <h5 className="d-inline ms-3" id="offcanvasRightLabel">Close</h5>
-                    <button type="button" className="btn-close btn-dark mt-2" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                  </div>
-                  <div className="w3-bar-block mt-1">
-                    
-                        <Link href="/#about" className="w3-bar-item w3-button " >ABOUT</Link>
-                        
-                        <Link href="/gallery" className="w3-bar-item w3-button" >
-                            <FontAwesomeIcon icon={faImage} style={{ fontSize: '1rem' }} className="me-2" /> GALLERY
-                        </Link> 
-                        
-                        <Link href="/#packages" className="w3-bar-item w3-button" >
-                            <FontAwesomeIcon icon={faTh} style={{ fontSize: '1rem' }} className="me-2" /> PACKAGES
-                        </Link>
-                        <Link href="/#contact" className="w3-bar-item w3-button" >
-                            <FontAwesomeIcon icon={faEnvelope} style={{ display: 'inline' }} className="me-2" /> CONTACT
-                        </Link>
-                    </div>
-                  </div>
-                </div>
-            </div>
-        </main>
-    )
+  return (
+    <main>
+      <nav className="navbar navbar-expand-lg">
+          <Link href="/#home" className="w3-bar-item w3-wide">
+            <Image
+              className="animate"
+              src={Logo}
+              alt="Logo"
+              width={111}
+              position="fixed"
+              top={0}
+              left={0}
+            />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <Hamburger direction="right" />
+          </button>
+        <div className="container-fluid">
+          <div className="collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link href="/#about" className="w3-bar-item w3-button me-2">ABOUT</Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/gallery" className="w3-bar-item w3-button">
+                  <FontAwesomeIcon icon={faImage} style={{ fontSize: '1rem' }} className="me-2" /> GALLERY
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/#packages" className="w3-bar-item w3-button">
+                  <FontAwesomeIcon icon={faTh} style={{ fontSize: '1rem' }} className="me-2" /> PACKAGES
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/#contact" className="w3-bar-item w3-button">
+                  <FontAwesomeIcon icon={faEnvelope} style={{ display: 'inline' }} className="me-2" /> CONTACT
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </main>
+  );
 }
