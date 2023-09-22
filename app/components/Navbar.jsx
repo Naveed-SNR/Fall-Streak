8f2aa14a5655b83403c2b3baa86d8feb57e9faf9
Naveed-SNR <sheikhnaveed3660@gmail.com>
2023-09-22 20:50:20 +0530

    Improved logic for checking for active link, [WIP]

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -7,17 +7,16 @@ import Logo from '../../public/images/logo/Logo.svg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImage, faTh, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { Fade as Hamburger } from 'hamburger-react';
-import { motion } from 'framer-motion';
 
 
 export default function Navbar() {
 
   const [sidebar, setSidebar] = useState(false)
   const [isOpen, setOpen] = useState(false)
-  
-  const currentPath = usePathname()
 
-  const [activeLink, setActiveLink] = useState()
+  const [activeLink, setActiveLink] = useState('/')
+  const currentPath = usePathname()
+  
 
   const toggleMenu=()=> {
     setOpen(!isOpen)
@@ -26,8 +25,11 @@ export default function Navbar() {
   }
 
   const handleClick = () => {
-    setActiveLink(`${currentPath}#about`)
+
+    setActiveLink(currentPath + window.location.hash)
     console.log(activeLink)
+    console.log(currentPath)
+   
   }
   return (
     
@@ -46,18 +48,18 @@ export default function Navbar() {
               left={0}
             />
           </Link>
-          <div className="w3-right" id="navbarSupportedContent">
-                <Link href="/#about" onClick={handleClick} className={`w3-bar-item w3-button w3-round me-2 d-none d-lg-flex mt-3 ${activeLink === '/#about' ? 'active' : ''}`}>ABOUT</Link>
+          <div className="w3-right" id="navlinks">
+                <Link href="/#about" onClick={handleClick} className={`w3-bar-item w3-button w3-round me-2 d-none d-lg-flex mt-3 ${activeLink === '/#about' ? 'active-nav' : ''}`}>ABOUT</Link>
 
-                <Link href="/gallery" className={`w3-bar-item w3-button w3-round me-2 d-none d-lg-flex mt-3 ${currentPath === '/#gallery' ? 'active' : ''}`}>
+                <Link href="/gallery" onClick={handleClick} className={`w3-bar-item w3-button w3-round me-2 d-none d-lg-flex mt-3 ${currentPath === '/gallery' ? 'active-nav' : ''}`}>
                   <FontAwesomeIcon icon={faImage} style={{ fontSize: '1rem' }} className="me-2 mt-1" /> GALLERY
                 </Link>
 
-                <Link href="/#packages" className={`w3-bar-item w3-button w3-round me-2 d-none d-lg-flex mt-3 ${currentPath === '/#packages' ? 'active' : ''}`}>
+                <Link href="/#packages" onClick={handleClick} className={`w3-bar-item w3-button w3-round me-2 d-none d-lg-flex mt-3 ${activeLink === '/#packages' ? 'active-nav' : ''}`}>
                   <FontAwesomeIcon icon={faTh} style={{ fontSize: '1rem' }} className="me-2 mt-1" /> PACKAGES
                 </Link>
 
-                <Link href="/#contact" className={`w3-bar-item w3-button w3-round me-2 d-none d-lg-flex mt-3 ${currentPath === '/#contact' ? 'active' : ''}`}>
+                <Link href="/#contact" onClick={handleClick} className={`w3-bar-item w3-button w3-round me-2 d-none d-lg-flex mt-3 ${activeLink === '/#contact' ? 'active-nav' : ''}`}>
                   <FontAwesomeIcon icon={faEnvelope} style={{ display: 'inline' }} className="me-2 mt-1" /> CONTACT
                 </Link>
                 <div className="d-lg-none d-block mt-2" onClick={toggleMenu}>
@@ -82,9 +84,7 @@ export default function Navbar() {
               <Link href="/#contact" className="w3-bar-item w3-button" onClick={toggleMenu}>
                 <FontAwesomeIcon icon={faEnvelope} style={{ display: 'inline' }} className="me-2" /> CONTACT
               </Link>
-
             </div>
-
       </div>
       </nav>
     </>
