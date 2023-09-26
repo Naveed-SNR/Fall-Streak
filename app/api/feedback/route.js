45a6284733bed42c4674b56514cf5f0da95b3a14
Naveed-SNR <sheikhnaveed3660@gmail.com>
2023-09-26 21:31:39 +0530

    Added styles for emails sent using nodemailer

--- app/api/feedback/route.js
+++ app/api/feedback/route.js
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 export async function POST(request)  {
-
 const email = process.env.EMAIL
 const password = process.env.EMAIL_PASSWORD
 try {
+
 
   const { name, emailID, subject, message} = await request.json();
   
@@ -19,12 +19,28 @@ try {
   const mailOptions = {
     from: email,
     to: email,
-    subject: 'New Message From Customer',
+    subject: 'Message from customer',
     html: `
-    <h3>Customer Name: ${name}</h3>
-    <li>Customer E-mail: ${emailID}</li>
-    <li>Subject: ${subject}</li> 
-    <li>Message: <br>${message}</li>
+        
+          
+            <div style="border: 1px solid grey; border-radius: 8.3px;">
+              <div style="background-color: black; color: white; padding: 11px; margin-top: 11px; border-bottom: 1px solid grey">
+                <h1 style="display: inline; font-size: 30px; font-family: Arial;">Fall Streak</h1><br><br><h2 style ="display: inline; font-family: Arial; font-size: 21px; font-weight: 400;">Message from customer</h2>
+              </div>
+              <div style="padding: 4px; margin: 11px;">
+                <h2 style ="display: inline; font-family: Arial; font-size: 21px; font-weight: 542;">Name:</h2><h2 style ="display: inline; font-size: 21px; font-weight:308; font-family: Arial;"> ${name}</h2>
+              </div>
+              <div style="padding: 4px; margin: 11px;">
+                <h2 style ="display: inline; font-family: Arial; font-size: 21px; font-weight: 542;">Email:</h2><h2 style ="display: inline; font-size: 21x; font-weight:308; font-family: Arial;"> ${emailID}</h2>
+              </div>
+              <div style="padding: 4px; margin: 11px;">
+                <h2 style ="display: inline; font-family: Arial; font-size: 21px; font-weight: 542;">Subject:</h2><h2 style ="display: inline; font-size: 21px; font-weight:308; font-family: Arial;"> ${subject}</h2>
+              </div>
+              <div style="padding: 4px; margin: 11px;">
+                <h2 style ="display: inline; font-family: Arial; font-size: 21px; font-weight: 542;">Message:</h2><h2 style ="display: inline; font-size: 21px; font-weight:308; font-family: Arial;"> ${message}</h2>
+              </div>
+            </div>
+        
     `
   };
     
